b0f7b020da1c92e2aa25994be2aecccc69e1d904
HAYANKRUM <devhayan03to@gmail.com>
2026-09-01 13:56:47 -0300

    projeto limpo

--- src/app/testes/push/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const TestesPushContent = dynamic(() => import('./TestesPushContent'), { ssr: false })
-
-export default function TestesPushPage() {
-  return <TestesPushContent />
-}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,13 +44,6 @@ export default function Navbar() {
           </svg>
         </Link>
         <div className="ml-auto flex items-center gap-4">
-          <Link href="/testes" className="transition-colors" style={{ color: 'var(--text-secondary)' }} title="Testes">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/>
-              <polyline points="14 2 14 8 20 8"/>
-              <path d="m9 15 2 2 4-4"/>
-            </svg>
-          </Link>
           {loaded && usuario && <SinoNotificacoes />}
           {loaded && usuario ? (
             <Link
